9aed4ca1f359f479cb3a1e445fdf16dc060912cd
BerkinAKKAYA <berkin_akkaya@hotmail.com>
2021-03-26 21:10:12 +0000

    Refactor Indentation

--- src/config.py
+++ src/config.py
@@ -1,38 +1,40 @@
 STYLES = {
-    "FILE": ["", "BLUE"],
-    "FOLDER": ["", "GREEN"],
+    "FILE":     ["", "BLUE"],
+    "FOLDER":   ["", "GREEN"],
 
-    "mp4": ["", "RED"],
-    "mkv": ["", "RED"],
+    "mp4":      ["", "RED"],
+    "mkv":      ["", "RED"],
 
-    "mp3": ["", "YELLOW"],
+    "mp3":      ["", "YELLOW"],
 
-    "jpg": ["", "VIOLET"],
-    "png": ["", "VIOLET"],
+    "jpg":      ["", "VIOLET"],
+    "png":      ["", "VIOLET"],
 
-    "html": ["", "BEIGE"],
-    "css": ["", "BEIGE"],
-    "js": ["", "BEIGE"],
+    "html":     ["", "BEIGE"],
+    "css":      ["", "BEIGE"],
+    "js":       ["", "BEIGE"],
 
-    "pdf": ["", "YELLOW"],
-    "srt": ["", "YELLOW"],
+    "pdf":      ["", "YELLOW"],
+    "srt":      ["", "YELLOW"],
 }
 
 COLORS = {
-    "BLACK"  : '\33[30m',
-    "GREY"   : '\33[90m',
-    "WHITE"  : '\33[37m',
-    "WHITE2" : '\33[97m',
-    "RED"    : '\33[31m',
-    "RED2"   : '\33[91m',
-    "GREEN"  : '\33[32m',
-    "GREEN2" : '\33[92m',
-    "YELLOW" : '\33[33m',
-    "YELLOW2" : '\33[93m',
-    "BLUE"   : '\33[34m',
-    "BLUE2"  : '\33[94m',
-    "VIOLET" : '\33[35m',
-    "VIOLET2" : '\33[95m',
-    "BEIGE"  : '\33[36m',
-    "BEIGE2" : '\33[96m',
+    "BLACK"     : '\33[30m',
+    "GREY"      : '\33[90m',
+    "WHITE"     : '\33[37m',
+
+    "RED"       : '\33[31m',
+    "GREEN"     : '\33[32m',
+    "YELLOW"    : '\33[33m',
+    "BLUE"      : '\33[34m',
+    "VIOLET"    : '\33[35m',
+    "BEIGE"     : '\33[36m',
+
+    "WHITE2"    : '\33[97m',
+    "RED2"      : '\33[91m',
+    "GREEN2"    : '\33[92m',
+    "YELLOW2"   : '\33[93m',
+    "BLUE2"     : '\33[94m',
+    "VIOLET2"   : '\33[95m',
+    "BEIGE2"    : '\33[96m',
 }
